7bf0cadafbeca597299e4e42314632431de450cb
Jan Panschab <jan.panschab@gmail.com>
2012-06-02 12:40:20 +0200

    + added geolocation

--- www/js/core.js
+++ www/js/core.js
@@ -1,7 +1,50 @@
-var myOptions = {
-    center: new google.maps.LatLng(49.837982, 15.458887),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+var app = {
+    map: null,
+    DEFAULT_LAT: 49.837982,
+    DEFAULT_LNG: 15.458887,
+    
+    init: function() {
+        var myOptions = {
+            center: new google.maps.LatLng(app.DEFAULT_LAT, app.DEFAULT_LNG),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        app.map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+        
+        app.geolocation();
+    },
+    geolocation: function() {
+        // Try W3C Geolocation (Preferred)
+        if (navigator.geolocation) {
+            var browserSupportFlag = true;
+            
+            navigator.geolocation.getCurrentPosition(function(position) {
+                console.log(position);
+                var initialLocation = new google.maps.LatLng(
+                        position.coords.latitude, 
+                        position.coords.longitude
+                    );
+                
+                app.map.setCenter(initialLocation);
+                
+            }, function() {
+                handleNoGeolocation(browserSupportFlag);
+            });
+
+        // Browser doesn't support Geolocation
+        } else {
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
+        }
+    },
+    handleNoGeolocation: function (errorFlag) {
+        if (errorFlag == true) {
+            alert('Geolocation service failed.');
+        } else {
+            alert('Your browser doesn\'t support geolocation.');
+        }
+        map.setCenter(new google.maps.LatLng(app.DEFAULT_LAT, app.DEFAULT_LNG));
+    }
 };
-var map = new google.maps.Map(document.getElementById('map-canvas'),
-    myOptions);
+
+app.init();
