805ce9a0b6d5073e595f35c5e33d0a9135de42fe
Almad <bugs@almad.net>
2012-06-02 18:27:11 +0200

    Fix link

--- scrapes/get_chmi_objects.py
+++ scrapes/get_chmi_objects.py
@@ -27,7 +27,7 @@ def get_station(link):
 
 
 def scrape():
-    complete_url = "http://hydro.chmi.cz/isarrow/objects.php?ukol_p=1&vod_typ=R&nadmh_sign=%3E&rickm_sign=%3E&rok_od=2007&rok_do=2012&objekty_chemdata=1&matrice=2000868184&typodb=41&seq=364787&ordrstr=NM&agenda=POV&limit_clsf=&matrice_clsf=&tscon_clsf=&rok_od_clsf=&rok_do_clsf=&val_sign_clsf=&val_clsf=&agg_clsf=&startpos=0&recnum=2770"
+    complete_url = "http://hydro.chmi.cz/isarrow/objects.php?ukol_p=1&vod_typ=R&nadmh_sign=%3E&rickm_sign=%3E&rok_od=2007&rok_do=2012&objekty_chemdata=1&matrice=2000868184&typodb=41&seq=364793&ordrstr=NM&agenda=POV&limit_clsf=&matrice_clsf=&tscon_clsf=&rok_od_clsf=&rok_do_clsf=&val_sign_clsf=&val_clsf=&agg_clsf=&startpos=0&recnum=2770"
     tree = fromstring(urllib2.urlopen(complete_url).read().decode('cp1250'))
     links = CSSSelector("table.tbl a")(tree)
     i = 1
