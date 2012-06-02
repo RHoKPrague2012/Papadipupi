e24be17d1c9d03b5d40a2a4891eb96ba403b766d
Jan Panschab <jan.panschab@gmail.com>
2012-06-02 12:01:43 +0200

    + added map

---
+++ www/js/core.js
@@ -0,0 +1,7 @@
+var myOptions = {
+    center: new google.maps.LatLng(49.837982, 15.458887),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById('map-canvas'),
+    myOptions);
