fc55c9ad0d1c86688db804ee2b1e3a1381750fa6
tliska <tliska@actum.cz>
2012-06-03 16:43:49 +0200

    + added names of sustances over limit

--- scrapes/gen_values.php
+++ scrapes/gen_values.php
@@ -16,17 +16,34 @@ $cnt = $argv[1];
 $limits = file_get_contents('limits.json');
 $limits = json_decode($limits);
 
+$names = array();
+foreach ($limits as $name => $value) {
+	$names[] = $name;
+}
+echo join(',', $names);
+echo "\n";
+
 for ($i = 0; $i < $cnt; $i++) {
+
+	$over = array();
 	foreach ($limits as $name => $value) {
 
 		if (!empty($value)) {
 			$modifier = get_modifier($value);
 			$rnd = rand(0, 2 * $modifier * $value);
-			echo $rnd/$modifier;
+
+			$val = $rnd/$modifier;
+			if ($val >= $value) {
+				$over[] = $name;
+			}
+
+			echo $val;
 		}
 
 		echo ',';
 	}
+
+	echo '"'.join(',', $over).'"';
 
 	echo "\n";
 }
